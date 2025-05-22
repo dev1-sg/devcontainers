8d01a26f39a2fdc41f1cf39c50e12dc07cce2166
antyung47 <antyung47@gmail.com>
2025-05-22 19:10:40 +0000

    add python feature

---
+++ src/features/python/install.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -exo pipefail
+
+VERSION=${VERSION:-"3.13.3"}
+
+function install_deps() {
+    install \
+        sudo \
+        wget \
+        ca-certificates \
+        xz-utils
+}
+
+function install() {
+    $(which sudo) apt-get update
+    export DEBIAN_FRONTEND=noninteractive
+    $(which sudo) apt-get install -y --no-install-recommends "$@"
+    sudo add-apt-repository ppa:deadsnakes/ppa -y
+    sudo apt-get update
+}
+
+function install_python() {
+    sudo apt install python3.13
+    pip install --no-cache-dir --upgrade pip \
+    && pip install --no-cache-dir poetry
+}
+
+function main() {
+    install_deps
+    install_python
+    sudo apt-get clean
+    sudo rm -rf /var/lib/apt/lists/*
+}
+
+main
