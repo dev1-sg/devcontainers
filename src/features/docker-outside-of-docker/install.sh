26bd2b74f519493168734284e2ba6dfe63ade59c
antyung47 <antyung47@gmail.com>
2025-07-26 17:28:36 +0800

    update

--- src/features/docker-outside-of-docker/install.sh
+++ src/features/docker-outside-of-docker/install.sh
@@ -26,7 +26,8 @@ function main() {
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/$ID \
     $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    DEBIAN_FRONTEND=noninteractive $(which sudo) apt-get install -y --no-install-recommends \
+    sudo apt-get update
+    DEBIAN_FRONTEND=noninteractive sudo apt-get install -y --no-install-recommends \
     docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 }
 
